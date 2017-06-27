1baa00a21161765ec59b8385fae89870c19e6d1d
GrokN <FullGrok@gmail.com>
2017-06-26 17:31:50 -0700

    Implement getting user token and hitting endpoint for user details

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -21,6 +21,14 @@ class App extends React.Component {
     })
   }
 
+  gitHubSignIn() {
+
+    let clientId = '0e43b859aea47726fa84';
+    let scopes = 'user';
+    window.open(`https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${scopes}`);
+    // window.open(`https://github.com/login/oauth/authorize?client_id=your-client-id`);
+  }
+
   addUser() {
     let data = {name: 'beth', email: 'beth@gmail.com', gitHub: '14hj3kl3h4k1h4k3'};
 
@@ -58,6 +66,7 @@ class App extends React.Component {
   render () {
     return (<div>
       <h1>users</h1>
+      <button onClick={this.gitHubSignIn.bind(this)}> Sign in with GitHub</button>
       <Users users={this.state.users}  addUser={this.addUser.bind(this)} />
     </div>)
   }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,67 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var db = require('../database-mysql/queries');
+var gitHub = require('../server/github.config')
+var request = require('request');
+var queryString = require('querystring');
 
 var app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 app.use(bodyParser.json()); // augment the req with body property which will have json from the post's body
 app.use(bodyParser.urlencoded());
+
+app.get('/login*', function (req, res) {
+
+  let url = 'https://github.com/login/oauth/access_token';
+  var formData = {
+    client_id: gitHub.clientId,
+    client_secret: gitHub.clientSecret,
+    code: req.query.code
+  };
+
+  // get auth token from github
+  request.post({url, formData}, (err, response, body) => {
+    let token = queryString.parse(body);
+
+    var options = {
+      url: 'https://api.github.com/user',
+      // url: 'https://api.github.com/user/emails',
+      headers: {
+        'User-Agent': 'request',
+        'Accept': 'application/json',
+        'Authorization': `token ${token.access_token}`
+      }
+    };
+
+    request.get(options, (err, response, body) => {
+      let userOptions = JSON.parse(body);
+      console.log('some response');
+      res.status(200).end();
+    });
+
+    // get user email and handle using access token
+    // check if in database and save / login accordingly.
+  });
+
+});
+
+// Accept-Language: en-us
+// Accept: application/json
+// Authorization: token 83f42..xxx
+// Accept-Encoding: gzip, deflate
+//
+// GET https://api.github.com/user/emails
+
+// access_token
+//   :
+//   "f62ffa3a0b2ef73b55e8e935e41ae0f67e5772f4"
+// scope
+//   :
+//   "user"
+// token_type
+//   :
+//   "bearer"
 
 app.get('/users', function (req, res) {
   db.selectAll()
