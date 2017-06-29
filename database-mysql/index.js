811fa042db9b9968ccc1adb0cc94c9479d0b6219
interdigitize <interdigitize@users.noreply.github.com>
2017-06-29 08:42:42 -0700

    Adds variable to database index to connect to deployment db or local db.

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -1,19 +1,20 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var connectTo = process.env.JAWSDB_URL || {
   host     : 'localhost',
   user     : 'root',
   password : 'chon',
   database : 'gravitas'
-});
+};
 
-// connection.connect(function(err) {
-//   if (err) {
-//     console.log('Error connecting to mysql database');
-//     throw err;
-//   }
-//
-//   console.log("Connected to mysql database!");
-// });
+var connection = mysql.createConnection(connectTo);
+
+connection.connect(function(err) {
+  if (err) {
+    console.log('Error connecting to mysql database');
+    throw err;
+  }
+  console.log("Connected to mysql database!");
+});
 
 module.exports = connection;
